8ebec0c7cf88beb3f0400d16fb655253e401a829
geovaneMT <gmtgeovane@gmail.com>
2023-12-20 17:17:21 -0300

    buttons

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,28 +1,88 @@
 import styled from "styled-components"
+import { keyframes } from "styled-components"
+
+const showContent = keyframes`
+  from {
+    opacity: 0;
+    width: 0;
+  }
+  to {
+    opacity: 1;
+    display: flex;
+    width: inherit;
+  }
+`
 
 export const Container = styled.button`
   color: ${({ theme }) => theme.COLORS.WHITE};
   background: ${({ theme }) => theme.COLORS.GRADIENT};
-
   display: flex;
   align-items: center;
-
   gap: 2.4vw;
-
   width: 75%;
   height: 5.6rem;
-
-  padding: 0 3.2rem;
   margin: 1.6rem auto 0;
-
   border-radius: 20px;
-
   filter: opacity(1);
+
+  &:hover:before {
+    opacity: 1;
+  }
+
+  &::before,
+  &::after {
+    border-radius: inherit;
+    content: "";
+    opacity: 0;
+    position: absolute;
+    inset: 0;
+  }
+
+  &::before {
+    background: radial-gradient(
+      800px circle at var(--mouse-x) var(--mouse-y),
+      ${({ theme }) => theme.COLORS.CYAN},
+      transparent 40%
+    );
+    filter: opacity(0.2);
+    z-index: 3;
+  }
+
+  &::after {
+    background: radial-gradient(
+      600px circle at var(--mouse-x) var(--mouse-y),
+      ${({ theme }) => theme.COLORS.WHITE},
+      transparent 40%
+    );
+    z-index: 1;
+  }
+
+  > a {
+    position: absolute;
+    inset: 2px;
+    z-index: 2;
+    border-radius: inherit;
+    background: ${({ theme }) => theme.COLORS.GRADIENT};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    display: flex;
+    align-items: center;
+    gap: 2.4vw;
+    padding: 0 3.2rem;
+    height: 100%;
+    transition: all 0.3s ease-in-out;
+    width: 100%;
+    animation: ${showContent} 0.3s ease-in-out forwards;
+  }
+
+  span {
+    z-index: 3;
+  }
 
   p {
     transition: all 0.3s ease-in-out;
     opacity: 1;
     display: block;
+    z-index: 3;
 
     &:last-child {
       display: none;
@@ -31,6 +91,11 @@ export const Container = styled.button`
   }
 
   &:hover {
+    &::after {
+      animation: ${showContent} 0.3s ease-in-out forwards;
+      display: none;
+    }
+
     p {
       transition: all 0.3s ease-in-out;
       display: none;
@@ -41,22 +106,24 @@ export const Container = styled.button`
         opacity: 1;
       }
     }
-  }
 
-  > a {
-    color: ${({ theme }) => theme.COLORS.WHITE};
+    > a {
+      display: none;
+    }
 
-    display: flex;
-    align-items: center;
-    gap: 2.4vw;
-    padding: 0 3.2rem;
-    width: 100%;
+    > a::before {
+      content: "";
+      display: block;
+      opacity: 1;
+      position: absolute;
+      inset: 0;
+      border-radius: inherit;
+      animation: ${showContent} 0.3s ease-in-out forwards;
+      background: ${({ theme }) => theme.COLORS.BACKGROUND};
+    }
   }
 
   @media screen and (min-width: 769px) {
-
-    > a {
-      height: 100%;
-    }
+    width: 100%;
   }
 `
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -20,15 +20,7 @@ export const Container = styled.div`
       &:last-child > p {
         margin-bottom: 4vh;
       }
-
-      > button {
-        width: 100%;
-        background: ${({ theme }) => theme.COLORS.BACKGROUND};
-
-        &:hover:before {
-          opacity: 1;
-        }
-
+      button {
         &:nth-child(1) {
           grid-area: child1;
           height: 100%;
@@ -42,43 +34,6 @@ export const Container = styled.div`
         }
         &:nth-child(4) {
           grid-area: child4;
-        }
-
-        &::before,
-        &::after {
-          border-radius: inherit;
-          content: "";
-          opacity: 0;
-          position: absolute;
-          inset: 0;
-        }
-
-        &::before {
-          background: radial-gradient(
-            800px circle at var(--mouse-x) var(--mouse-y),
-            ${({ theme }) => theme.COLORS.CYAN},
-            transparent 40%
-          );
-          filter: opacity(0.2);
-          z-index: 3;
-        }
-
-        &::after {
-          background: radial-gradient(
-            600px circle at var(--mouse-x) var(--mouse-y),
-            ${({ theme }) => theme.COLORS.WHITE},
-            transparent 40%
-          );
-          z-index: 1;
-          opacity: 0;
-        }
-
-        > a {
-          inset: 2px;
-          position: absolute;
-          background: ${({ theme }) => theme.COLORS.GRADIENT};
-          z-index: 2;
-          border-radius: inherit;
         }
       }
     }
